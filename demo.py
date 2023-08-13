cd48a754c6c70573eac12e3ddda49cf09116b13a
root <root@DESKTOP-0T01DAB>
2023-08-13 16:00:56 +0800

    implement create_logical_switch && create_router_port method

--- demo.py
+++ demo.py
@@ -12,15 +12,18 @@ c = connection.Connection(idl=i, timeout=3)
 # The OVN_Northbound API implementation object
 api = impl_idl.OvnNbApiIdlImpl(c)
 
-api.ls_add("sw0").execute()
-with api.transaction(check_error=True) as txn:
-    txn.add(api.ls_add("sw1"))
-    txn.add(api.lsp_add("sw1", "sw1-port1"))
-    txn.add(api.lsp_set_addresses("sw1-port1", ["50:54:00:00:00:01 11.0.0.1"]))
-    txn.add(api.lsp_add("sw1", "sw1-port2"))
-    txn.add(api.lsp_set_addresses("sw1-port2", ["50:54:00:00:00:02 11.0.0.2"]))
+try:
+    api.ls_add("sw0").execute(check_error=True)
+except :
+    print("error")
+# with api.transaction(check_error=True) as txn:
+#     txn.add(api.ls_add("sw1"))
+#     txn.add(api.lsp_add("sw1", "sw1-port1"))
+#     txn.add(api.lsp_set_addresses("sw1-port1", ["50:54:00:00:00:01 11.0.0.1"]))
+#     txn.add(api.lsp_add("sw1", "sw1-port2"))
+#     txn.add(api.lsp_set_addresses("sw1-port2", ["50:54:00:00:00:02 11.0.0.2"]))
 
-for ls_row in api.ls_list().execute(check_error=True):
-    print("uuid: %s, name: %s" % (ls_row.uuid, ls_row.name))
-    for lsp_row in api.lsp_list(switch=ls_row.uuid).execute():
-        print("  uuid: %s, name: %s" % (lsp_row.uuid, lsp_row.name))
+# for ls_row in api.ls_list().execute(check_error=True):
+#     print("uuid: %s, name: %s" % (ls_row.uuid, ls_row.name))
+#     for lsp_row in api.lsp_list(switch=ls_row.uuid).execute():
+#         print("  uuid: %s, name: %s" % (lsp_row.uuid, lsp_row.name))
--- subnet.py
+++ subnet.py
@@ -1,3 +1,6 @@
+import subprocess
+
+
 def create_subnet():
     pass
 
@@ -15,14 +18,51 @@ def check_subnet_conflict():
 
 
 def create_logical_switch(
-    ls_name: str, lr_name: str, cidr_block: str, gateway: str
+    ls_name: str, lr_name: str, cidr_block: str, gateway: str, need_router=True
 ) -> None:
-    pass
+    # TODO:need log to record
+    status, output = subprocess.getstatusoutput(
+        "ovn-nbctl ls-add {0}-{1}".format(lr_name, ls_name)
+    )
+    if not status:
+        print(f"create logical switch {lr_name} success...")
+    else:
+        print(output)
+
+    if need_router:
+        create_router_port(ls_name, lr_name)
 
 
-class Subnet:
-    pass
+def create_router_port(ls_name: str, lr_name: str) -> None:
+    # TODO:need log to record
+
+    # create logical router
+    status, output = subprocess.getstatusoutput(f"ovn-nbctl lr-add {lr_name}")
+    if not status:
+        print(f"create logical router {lr_name} success...")
+    else:
+        print(output)
+
+    # create logical router port
+    router_port = "{0}-{1}-port".format(lr_name, ls_name)
+    status, output = subprocess.getstatusoutput(
+        f"ovn-nbctl lrp-add {lr_name} {router_port} 52:54:00:c1:68:50 10.0.0.1/24"
+    )
+    if not status:
+        print(f"create logical router {router_port} success...")
+    else:
+        print(output)
+
+    # create logical switch port patch to router
+    port_patch_to_router = "{0}-{1}-port".format(ls_name, lr_name)
+    status, output = subprocess.getstatusoutput(
+        f"ovn-nbctl lsp-add {lr_name}-{ls_name} {port_patch_to_router} -- set logical_switch_port {port_patch_to_router} type=router options:router-port={router_port}"
+    )
+    if not status:
+        print("create switch port patch to router success...")
+    else:
+        print(output)
 
 
 if __name__ == "__main__":
-    pass
+    create_logical_switch("subnet1", "vpc1", "10.10.10.10/24", "10.10.10.1")
